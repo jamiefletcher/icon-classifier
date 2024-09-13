cff59d3b5c586aa7aaa13608d89a6f661eba6f03
Jamie Fletcher <68712911+jamiefletcher@users.noreply.github.com>
2024-09-13 15:52:53 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 from .download import download
 from .process import process
+from make_dataset import make_dataset
 
 __version__ = "0.1.0"
 
 # Public interface when module is imported
-__all__ = ['download', 'process']
+__all__ = ['download', 'process', 'make_dataset']
